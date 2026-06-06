4670d828c3c9da139c95072df218dc4f5017831c
cora4 <80816140+cora4@users.noreply.github.com>
2026-06-06 10:24:53 +0000

    Update build.gradle.kts

--- smollm/build.gradle.kts
+++ smollm/build.gradle.kts
@@ -39,7 +39,7 @@ android {
                 arguments += "-DCMAKE_VERBOSE_MAKEFILE=ON"
 
                 arguments += "-DBUILD_SHARED_LIBS=ON"
-                arguments += "-DLLAMA_BUILD_COMMON=ON"
+                arguments += "-DLLAMA_BUILD_COMMON=OFF"
                 arguments += "-DLLAMA_CURL=OFF"
                 arguments += "-DGGML_LLAMAFILE=OFF"
                 // (debugging) uncomment the following line to enable debug builds
